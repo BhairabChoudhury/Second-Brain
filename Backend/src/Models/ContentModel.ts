b9fd569369add495f2402e7fe04e304c1eb249d9
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2026-03-26 02:59:22 +0530

    set

---
+++ Backend/src/Models/ContentModel.ts
@@ -0,0 +1,26 @@
+import mongoose , { Schema , Document}from "mongoose";
+
+export interface IContent extends Document{
+    title:string ;
+    type : "note" | "link" | "pdf" | "image" ;
+    content?:string ;
+    fileUrl?:string ; 
+    extractedText?:string ;  
+    userId :mongoose.Schema.Types.ObjectId;
+    createdAt:Date ;
+    updatedAt:Date ;  
+}
+const ContentSchema = new Schema<IContent>({
+  title :{ type :String , required:true} ,
+  type :{ type :String , enum:["note","link","pdf","image"] , required:true} ,
+  content :{ type :String  } ,
+  fileUrl :{ type :String  } ,
+  extractedText :{ type :String  } , 
+  userId :{ type :mongoose.Schema.Types.ObjectId , ref:"User" , required:true} 
+}, {
+    timestamps: true
+})
+
+const ContentModel = mongoose.model<IContent>("Content", ContentSchema)
+
+export default ContentModel; 
--- Backend/src/Models/UserModel.ts
+++ Backend/src/Models/UserModel.ts
@@ -7,6 +7,8 @@ username :{type :String , required :true } ,
 email :{type :String , required :true } ,  
 password :{type :String , required :true  }
 
+} ,{
+    timestamps:true  
 })
 
 const UserModel  = mongoose.model("User", UserSchema) ;
